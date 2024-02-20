9a722bbe3b309bd2f6fd3f85587ce9d7f0168083
David | Wolf Worldwide <wolf.translations2021@gmail.com>
2024-02-20 02:27:05 +0100

    Changes to be committed:         new file:   public/images/logo-noname.webp 	renamed:    src/assets/images/3 - Copy.webp -> src/assets/images/3.webp 	new file:   src/assets/images/logo-2.webp 	modified:   src/components/common/Logo.tsx

--- src/components/common/Logo.tsx
+++ src/components/common/Logo.tsx
@@ -1,16 +1,16 @@
 import { component$ } from "@builder.io/qwik";
 
 // @ts-ignore
-import logoSrc from "~/assets/images/logo.webp";
+import logoSrc from "~/assets/images/logo-2.webp";
 
 export default component$(() => (
   <span class="self-center ml-2 text-2xl md:text-xl font-bold text-gray-900 whitespace-nowrap dark:text-white flex items-center">
     <img
       src={logoSrc}
       class="inline-block mr-1"
-      width={32}
-      height={32}
-      alt="Qwind Logo"
+      width={96}
+      height={96}
+      alt="AI-Assist Logo"
       loading="lazy"
     />
     AI-Assist
